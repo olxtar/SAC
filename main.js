469bf21ede2181dd30b1b6b540dd6f9ce67d47d2
olxtar <73145844+olxtar@users.noreply.github.com>
2024-09-12 14:43:55 +0900

    main.js

--- main.js
+++ main.js
@@ -1,98 +1,98 @@
 var getScriptPromisify = (src) => {
-	return new Promise((resolve) => {
-		$.getScript(src, resolve)
-	})
+  return new Promise((resolve) => {
+    $.getScript(src, resolve)
+  })
 }
 
 var parseMetadata = metadata => {
-	const { dimensions: dimensionsMap, mainStructureMembers: measuresMap } = metadata
-	const dimensions = []
-	for (const key in dimensionsMap) {
-		const dimension = dimensionsMap[key]
-		dimensions.push({ key, ...dimension })
-	}
-	const measures = []
-	for (const key in measuresMap) {
-		const measure = measuresMap[key]
-		measures.push({ key, ...measure })
-	}
-	return { dimensions, measures, dimensionsMap, measuresMap }
+  const { dimensions: dimensionsMap, mainStructureMembers: measuresMap } = metadata
+  const dimensions = []
+  for (const key in dimensionsMap) {
+    const dimension = dimensionsMap[key]
+    dimensions.push({ key, ...dimension })
+  }
+  const measures = []
+  for (const key in measuresMap) {
+    const measure = measuresMap[key]
+    measures.push({ key, ...measure })
+  }
+  return { dimensions, measures, dimensionsMap, measuresMap }
 }
 
+(function () {
+  const template = document.createElement('template')
+  template.innerHTML = `
+        <style>
+        </style>
+        <div id="root" style="width: 100%; height: 100%;">
+        </div>
+      `
+  class Main extends HTMLElement {
+    constructor () {
+      super()
 
-	(function () {
-		const template = document.createElement('template')
-		template.innerHTML = `
-			<style>
-			</style>
-			<div id="root" style="width: 100%; height: 100%;">
-			Hello Custom Widget !!!!
-			</div>
-		`
-		class Main extends HTMLElement {
-			constructor () {
-			super()
-				
-			this._shadowRoot = this.attachShadow({ mode: 'open' })
-			this._shadowRoot.appendChild(template.content.cloneNode(true))
-				
-			this._root = this._shadowRoot.getElementById('root')
+      this._shadowRoot = this.attachShadow({ mode: 'open' })
+      this._shadowRoot.appendChild(template.content.cloneNode(true))
 
-			this._eChart = null
-			}
+      this._root = this._shadowRoot.getElementById('root')
 
-			onCustomWidgetResize (width, height) {
-				this.render()
-			}
+      this._eChart = null
+    }
 
-			onCustomWidgetAfterUpdate (changedProps) {
-				this.render()
-			}
+    onCustomWidgetResize (width, height) {
+      this.render()
+    }
 
-			onCustomWidgetDestroy () {
-			}
+    onCustomWidgetAfterUpdate (changedProps) {
+      this.render()
+    }
 
-			async render () {
-				const dataBinding = this.dataBinding
-				if (!dataBinding || dataBinding.state !== 'success') { return }
 
-				await getScriptPromisify('https://cdn.staticfile.org/echarts/5.0.0/echart.min.js)
+    onCustomWidgetDestroy () {
 
-				const { data, metadata } = dataBinding
-				const { dimensions, measures } = parseMetadata(metadata)
-				// dimension
-				const categoryData = []
-				// measures
-				const series = measures.map(measure => { 
-					return {
-						id: measure.id,
-						name: measure.label,
-						data: [],
-						key: measure.key,
-						type: 'line',
-						smooth: true
-					}
-				})
-				data.forEach(row => {
-					categoryData.push(dimensions.map(dimension => {
-						return row[dimension.key].label
-					}).join('/')) 
-					series.forEach(series => {
-						series.data.push(row[series.key].raw)
-					})
-				})
+  }
 
-				if (this._eChart) { echarts.dispose(this._eChart) }
-				const eChart = this._eChart = echarts.init(this._root, 'main')
-				const option = {
-					xAxis: { type: 'category', data: categoryData },
-					yAxis: { type: 'value' },
-					tooltip: { trigger: 'axis'},
-					series
-				}
-				eChart.setOption(option)		
-			}
-		}
-			
-		customElements.define('com-sap-sac-exercise-olxtar-main', Main)
-	})()
+    async render () {
+      const dataBinding = this.dataBinding
+      if (!dataBinding || dataBinding.state !== 'success') { return }
+
+      await getScriptPromisify('https://cdn.staticfile.org/echarts/5.0.0/echarts.min.js')
+
+      const { data, metadata } = dataBinding
+      const { dimensions, measures } = parseMetadata(metadata)
+      // dimension
+      const categoryData = []
+      // measures
+      const series = measures.map(measure => {
+        return {
+          id: measure.id,
+          name: measure.label,
+          data: [],
+          key: measure.key,
+          type: 'line',
+          smooth: true
+        }
+      })
+      data.forEach(row => {
+        categoryData.push(dimensions.map(dimension => {
+          return row[dimension.key].label
+        }).join('/')) // dimension
+        series.forEach(series => {
+          series.data.push(row[series.key].raw)
+        }) // measures
+      })
+
+      if (this._eChart) { echarts.dispose(this._eChart) }
+      const eChart = this._eChart = echarts.init(this._root, 'main')
+      const option = {
+        xAxis: { type: 'category', data: categoryData },
+        yAxis: { type: 'value' },
+        tooltip: { trigger: 'axis' },
+        series
+      }
+      eChart.setOption(option)
+    }
+  }
+
+  customElements.define('com-sap-sac-exercise-olxtar-main', Main)
+})()
