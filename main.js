5de7fb62a6c34da807004d62632d6add857135bf
olxtar <73145844+olxtar@users.noreply.github.com>
2024-09-12 11:23:22 +0900

    main.js

--- main.js
+++ main.js
@@ -9,11 +9,11 @@
         `
     class Main extends HTMLElement {
         constructor () {
-            super()
+        super()
                 
-            this._shadowRoot = this.attachShadow({ mode: 'open' })
-            this._shadowRoot.appendChild(template.content.cloneNode(true))
-                this._root = this._shadowRoot.getElementById('root')
+        this._shadowRoot = this.attachShadow({ mode: 'open' })
+        this._shadowRoot.appendChild(template.content.cloneNode(true))
+        this._root = this._shadowRoot.getElementById('root')
         }
     }
     
